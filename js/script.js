70c0942921be86840ef50ca1197cdd29e896c0e7
sandeep kumar sahoo <chandansandeep2000@gmail.com>
2023-09-23 11:36:44 +0530

    Adding all files

---
+++ js/script.js
@@ -0,0 +1,407 @@
+
+function _0xcde5(){const _0x29bc92=['querySelectorAll','right','60px','classList','55928wzFNOv','header','Message\x20\x20Not\x20Sent\x20(Service\x20Error)\x20❌\x20💔😞','preventDefault','add','forEach','blur-header','#contact-form','Full-Stack\x20\x20Developer','.home__image','.nav__menu\x20a[href*=','template_xoljm0b','top','left','remove','show-menu','scroll-up','textContent','483eJUGvX','919441gopTKh','scrollY','contact-message','addEventListener','service_dwpdyt8','66NBhzqh','Web\x20Developer','getElementById','4120371LtnZdV','contact-form','112748PLyuIP','submit','nav-close','offsetHeight','680004XlkXZi','scroll','reveal','getAttribute','querySelector','show-scroll','then','390426gimoSo','nav-toggle','click','nav-menu','Message\x20Sent\x20Successfully\x20✅\x20💖','3383380lzRGjE','.home__data,.home__social,.contact__container,.footer__container','section[id]','reset','bottom'];_0xcde5=function(){return _0x29bc92;};return _0xcde5();}const _0x5d36cd=_0x3be7;(function(_0x1149c0,_0x280524){const _0x4e9be4=_0x3be7,_0x5b737a=_0x1149c0();while(!![]){try{const _0x54334d=parseInt(_0x4e9be4(0x96))/0x1+-parseInt(_0x4e9be4(0xa4))/0x2+parseInt(_0x4e9be4(0x9b))/0x3*(-parseInt(_0x4e9be4(0xa0))/0x4)+parseInt(_0x4e9be4(0xb0))/0x5+-parseInt(_0x4e9be4(0xab))/0x6+-parseInt(_0x4e9be4(0x95))/0x7*(parseInt(_0x4e9be4(0xb9))/0x8)+parseInt(_0x4e9be4(0x9e))/0x9;if(_0x54334d===_0x280524)break;else _0x5b737a['push'](_0x5b737a['shift']());}catch(_0x1fa754){_0x5b737a['push'](_0x5b737a['shift']());}}}(_0xcde5,0x85642));var typed=new Typed('.text',{'strings':['Frontend\x20Developer','Database\x20Administrator',_0x5d36cd(0x9c),_0x5d36cd(0xc1)],'typeSpeed':0x78,'backSpeed':0x50,'backDelay':0x384,'loop':!![],'showCursor':!![]});const navMenu=document[_0x5d36cd(0x9d)](_0x5d36cd(0xae)),navToggle=document[_0x5d36cd(0x9d)](_0x5d36cd(0xac)),navClose=document[_0x5d36cd(0x9d)](_0x5d36cd(0xa2));navToggle&&navToggle[_0x5d36cd(0x99)](_0x5d36cd(0xad),()=>{const _0x1c2286=_0x5d36cd;navMenu[_0x1c2286(0xb8)][_0x1c2286(0xbd)](_0x1c2286(0x92));});navClose&&navClose['addEventListener']('click',()=>{const _0x30d30e=_0x5d36cd;navMenu['classList'][_0x30d30e(0x91)](_0x30d30e(0x92));});const navLink=document[_0x5d36cd(0xb5)]('.nav__link'),linkAction=()=>{const _0x5c877b=_0x5d36cd,_0x329845=document[_0x5c877b(0x9d)]('nav-menu');_0x329845[_0x5c877b(0xb8)][_0x5c877b(0x91)](_0x5c877b(0x92));};navLink[_0x5d36cd(0xbe)](_0x179b36=>_0x179b36[_0x5d36cd(0x99)](_0x5d36cd(0xad),linkAction));const blurHeader=()=>{const _0x556c02=_0x5d36cd,_0x3e1419=document[_0x556c02(0x9d)](_0x556c02(0xba));this[_0x556c02(0x97)]>=0x32?_0x3e1419[_0x556c02(0xb8)]['add'](_0x556c02(0xbf)):_0x3e1419['classList'][_0x556c02(0x91)](_0x556c02(0xbf));};function _0x3be7(_0x519ed3,_0x3eed2e){const _0xcde530=_0xcde5();return _0x3be7=function(_0x3be764,_0x1952b4){_0x3be764=_0x3be764-0x91;let _0x562846=_0xcde530[_0x3be764];return _0x562846;},_0x3be7(_0x519ed3,_0x3eed2e);}window['addEventListener']('scroll',blurHeader);const contactForm=document['getElementById'](_0x5d36cd(0x9f)),contactMessage=document['getElementById'](_0x5d36cd(0x98)),sendEmail=_0x2d3380=>{const _0x6fea2a=_0x5d36cd;_0x2d3380[_0x6fea2a(0xbc)](),emailjs['sendForm'](_0x6fea2a(0x9a),_0x6fea2a(0xc4),_0x6fea2a(0xc0),'glw0_9giPKBN95rTz')[_0x6fea2a(0xaa)](()=>{const _0x242f23=_0x6fea2a;contactMessage[_0x242f23(0x94)]=_0x242f23(0xaf),setTimeout(()=>{const _0x31fc51=_0x242f23;contactMessage[_0x31fc51(0x94)]='';},0xbb8),contactForm[_0x242f23(0xb3)]();},()=>{const _0x4d5498=_0x6fea2a;contactMessage['textContent']=_0x4d5498(0xbb);});};contactForm[_0x5d36cd(0x99)](_0x5d36cd(0xa1),sendEmail);const scrollUp=()=>{const _0x3c64d1=_0x5d36cd,_0x420c84=document[_0x3c64d1(0x9d)](_0x3c64d1(0x93));this[_0x3c64d1(0x97)]>=0x15e?_0x420c84[_0x3c64d1(0xb8)][_0x3c64d1(0xbd)](_0x3c64d1(0xa9)):_0x420c84[_0x3c64d1(0xb8)][_0x3c64d1(0x91)](_0x3c64d1(0xa9));};window[_0x5d36cd(0x99)](_0x5d36cd(0xa5),scrollUp);const sections=document['querySelectorAll'](_0x5d36cd(0xb2)),scrollActive=()=>{const _0x3558a8=_0x5d36cd,_0x559f00=window[_0x3558a8(0x97)];sections[_0x3558a8(0xbe)](_0x854a8=>{const _0x1f3be1=_0x3558a8,_0x44a9b2=_0x854a8[_0x1f3be1(0xa3)],_0x551415=_0x854a8['offsetTop']-0x3a,_0x2809a6=_0x854a8[_0x1f3be1(0xa7)]('id'),_0x34dde0=document[_0x1f3be1(0xa8)](_0x1f3be1(0xc3)+_0x2809a6+']');_0x559f00>_0x551415&&_0x559f00<=_0x551415+_0x44a9b2?_0x34dde0['classList'][_0x1f3be1(0xbd)]('active-link'):_0x34dde0[_0x1f3be1(0xb8)]['remove']('active-link');});};window[_0x5d36cd(0x99)](_0x5d36cd(0xa5),scrollActive);const sr=ScrollReveal({'origin':_0x5d36cd(0xc5),'distance':_0x5d36cd(0xb7),'duration':0x9c4,'delay':0x190});sr[_0x5d36cd(0xa6)](_0x5d36cd(0xb1)),sr[_0x5d36cd(0xa6)](_0x5d36cd(0xc2),{'origin':_0x5d36cd(0xb4)}),sr['reveal']('.about__data,.skills__data',{'origin':_0x5d36cd(0xc6)}),sr['reveal']('.about__image,.skills__content',{'origin':_0x5d36cd(0xb6)}),sr[_0x5d36cd(0xa6)]('.services__card,.projects__card',{'interval':0x64});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var typed=new Typed(".text",{
+//     strings:["Frontend Developer", "Database Administrator","Web Developer", "Full-Stack  Developer"],
+//     typeSpeed:120,
+//     backSpeed:80,
+//     backDelay:900,
+//     loop:true,
+//     showCursor:true,
+
+//     // cursorChar:"_",
+//     });
+
+ 
+
+// /*=============== SHOW MENU ===============*/
+// const navMenu = document.getElementById('nav-menu'),
+//       navToggle = document.getElementById('nav-toggle'),
+//       navClose = document.getElementById('nav-close')
+
+// /*===== MENU SHOW =====*/
+// /* Validate if constant exists */
+// if(navToggle){
+//     navToggle.addEventListener('click', () =>{
+//         navMenu.classList.add('show-menu')
+//     })
+// }
+
+// /*===== MENU HIDDEN =====*/
+// /* Validate if constant exists */
+// if(navClose){
+//     navClose.addEventListener('click', () =>{
+//         navMenu.classList.remove('show-menu')
+//     })
+// }
+
+
+// /*=============== REMOVE MENU MOBILE ===============*/
+// const navLink = document.querySelectorAll('.nav__link')
+
+// const linkAction = () =>{
+//     const navMenu = document.getElementById('nav-menu')
+//     // When we click on each nav__link, we remove the show-menu class
+//     navMenu.classList.remove('show-menu')
+// }
+// navLink.forEach(n => n.addEventListener('click', linkAction))
+
+// /*=============== CHANGE BACKGROUND HEADER ===============*/
+
+// const blurHeader = () =>{
+//     const header = document.getElementById('header')
+//     // When the scroll is greater than 50 viewport height, add the blur-header class to the header tag
+//     this.scrollY >= 50 ? header.classList.add('blur-header') 
+//                        : header.classList.remove('blur-header')
+// }
+// window.addEventListener('scroll', blurHeader)
+
+// // email js
+
+// const contactForm=document.getElementById('contact-form'), 
+//       contactMessage=document.getElementById('contact-message')
+
+// const sendEmail=(e)=>{
+//     e.preventDefault()
+//     emailjs.sendForm('service_dwpdyt8', 'template_xoljm0b','#contact-form', 'glw0_9giPKBN95rTz')
+//     .then(()=>{
+//         // show sent message
+//         contactMessage.textContent='Message Sent Successfully ✅ 💖'
+
+//         //remove message after 3 sec
+//         setTimeout(()=>{
+//             contactMessage.textContent=''
+//         },3000)
+
+//         // clear input feilds
+//         contactForm.reset()
+
+//     }, ()=>{
+//         // show error Message
+//         contactMessage.textContent='Message  Not Sent (Service Error) ❌ 💔😞'
+
+//     })
+
+// }      
+ 
+// contactForm.addEventListener('submit', sendEmail);
+
+// /*=============== SHOW SCROLL UP ===============*/ 
+// const scrollUp = () =>{
+//  const scrollUp = document.getElementById('scroll-up')
+//     // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+//  this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+//                      : scrollUp.classList.remove('show-scroll')
+// }
+// window.addEventListener('scroll', scrollUp)
+
+// /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+// const sections = document.querySelectorAll('section[id]')
+    
+// const scrollActive = () =>{
+//      const scrollDown = window.scrollY
+
+//  sections.forEach(current =>{
+//      const sectionHeight = current.offsetHeight,
+//            sectionTop = current.offsetTop - 58,
+//            sectionId = current.getAttribute('id'),
+//            sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+//      if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+//          sectionsClass.classList.add('active-link')
+//      }else{
+//          sectionsClass.classList.remove('active-link')
+//      }                                                    
+//  })
+// }
+// window.addEventListener('scroll', scrollActive)
+
+// // scroll revel animation
+// const sr=ScrollReveal({
+//     origin:'top',
+//     distance:'60px',
+//     duration:2500,
+//     delay:400,
+//     //reset:true//animation is repet
+
+// })
+// sr.reveal(`.home__data,.home__social,.contact__container,.footer__container`)
+// sr.reveal(`.home__image`, {origin:'bottom'})
+// sr.reveal( `.about__data,.skills__data`,{origin:'left'})
+// sr.reveal(`.about__image,.skills__content`,{origin:'right'})
+// sr.reveal(`.services__card,.projects__card`,{interval:100})
+// // sr.reveal(`.contact__data,.contact__button`,{origin:'bottom'})
+
+
